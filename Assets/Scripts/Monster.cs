b5de105cffe6c10805cd310a53c293d6a169a7c8
Bskasan <b.kasan@yandex.com>
2024-07-02 13:45:57 +0300

    done - attributes

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
-    [SerializeField] private string _name;
+    [Header("General Stats")]
+    [Space]
+    [SerializeField] 
+    private string _name;
+
+    [SerializeField] [Range(0, 100)] 
+    private float _changeToDropItem;
+
+    [SerializeField] [Tooltip("Radius size where monster will see the player.")]
+    private float _rangeOfAwareness; 
+
+    [Header("Combat Stats")]
+    [Space]
+    [SerializeField] private int _damage;
+    [SerializeField] private int _health;
+    [SerializeField] private int _speed;
+
+    [Header("Dialogue")]
+    [SerializeField] [Tooltip("Speaks dialogue when entering combat")]
+    [TextArea()]
+    private string _battleCry;
+
+
 
 }
