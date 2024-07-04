6dd24ccaea787f67ec6d41d33721cd148a77a0c1
Bskasan <b.kasan@yandex.com>
2024-07-04 15:21:49 +0300

    up - added icons to editor and added on draw gizmos method for elected object

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -7,7 +7,16 @@ public class Monster : MonoBehaviour
     [SerializeField] private MonsterData _monsterData;
 
     private void Awake() {
-        Debug.Log("Name: " + _monsterData.GetNameValue());
-        Debug.Log("Damage: " + _monsterData.GetDamageValue());
+        Debug.Log("Name: " + _monsterData.Name);
+        Debug.Log("Damage: " + _monsterData.Damage);
+        Debug.Log("Monster Type: " + _monsterData.TypeOfMonster);
+        Debug.Log("Speed: " + _monsterData.Speed);
+        Debug.Log("Battle Cry: " + _monsterData.BattleCry);
+    }
+
+    // If selected...
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _monsterData.RangeOfAwareness);
     }
 }
