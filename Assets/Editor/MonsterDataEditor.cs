40d1d51984d15c1e48caf43ddd8dc32216b3615e
Bskasan <b.kasan@yandex.com>
2024-07-11 16:53:27 +0300

    up

--- Assets/Editor/MonsterDataEditor.cs
+++ Assets/Editor/MonsterDataEditor.cs
@@ -7,32 +7,60 @@ using Codice.Utils;
 [CustomEditor(typeof(MonsterData))]
 public class MonsterDataEditor : Editor
 {
+
+    private SerializedProperty _typeOfMonster;
+     
+    private SerializedProperty _name;
+    private SerializedProperty _battleCry;
+     
+    private SerializedProperty _changeToDropItem;
+    private SerializedProperty _rangeOfAwareness;
+     
+    private SerializedProperty _canEnterCombat;
+     
+    private SerializedProperty _damage;
+    private SerializedProperty _health;
+    private SerializedProperty _speed;
+
+    private void OnEnable() {
+        // Getting data from Monster Data
+        _name = serializedObject.FindProperty("_name");
+        _battleCry = serializedObject.FindProperty("_battleCry");
+        _changeToDropItem = serializedObject.FindProperty("_changeToDropItem");
+        _rangeOfAwareness = serializedObject.FindProperty("_rangeOfAwareness");
+        _canEnterCombat = serializedObject.FindProperty("_canEnterCombat");
+        _damage = serializedObject.FindProperty("_damage");
+        _health = serializedObject.FindProperty("_health");
+        _speed = serializedObject.FindProperty("_speed");
+        _typeOfMonster = serializedObject.FindProperty("_typeOfMonster");
+    }
+
     public override void OnInspectorGUI() {
 
         MonsterData data = (MonsterData)target;
 
-        string monsterTitleLabel = data.Name + " - " + data.TypeOfMonster;
+        string monsterTitleLabel = _name.stringValue.ToUpper() + " - " + _typeOfMonster.stringValue.ToUpper();
 
         EditorGUILayout.LabelField(monsterTitleLabel.ToUpper(), EditorStyles.boldLabel);
         EditorGUILayout.Space(10);
 
         // Difficulty Bar
-        float difficulty = data.Health + data.Damage + data.Speed;
+        float difficulty = _health.intValue + _damage.intValue + _speed.intValue;
         ProgressBar(difficulty / 100, SetDifficultyLevelOnInspector(difficulty));
 
         // Add before
-        //base.OnInspectorGUI();
+        
+        base.OnInspectorGUI();
+
+        // Redraw with our custom code...
 
         // Add after
 
         // Help Box
-        if (data.Name == string.Empty) {
+        if (_name.stringValue == string.Empty) {
             EditorGUILayout.HelpBox("Caution: No name specified. Please name the monster!", MessageType.Warning);
         }
 
-        if (data.TypeOfMonster == MonsterType.None) {
-            EditorGUILayout.HelpBox("No Monster type selected!", MessageType.Warning);
-        }
 
         if (data.Health <= 0) {
             EditorGUILayout.HelpBox("Shouldn't have a negative value for health!", MessageType.Warning);
@@ -62,4 +90,3 @@ public class MonsterDataEditor : Editor
         EditorGUI.ProgressBar(rect, value, label);
         EditorGUILayout.Space(10);
     }
-}
